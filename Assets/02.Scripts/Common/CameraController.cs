fccec59e3d42d3cefe895d7e9fddc37e0aaf9857
YesMan <kaka0804@naver.com>
2024-11-16 13:08:08 +0900

    Feat: 카메라 컨트롤러 수정

--- Assets/02.Scripts/Common/CameraController.cs
+++ Assets/02.Scripts/Common/CameraController.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float rotateValue = 15f;
+    private float _rotateValue = 15f;
+    private Vector3 _originRotation;
+
+    private bool isRotating = false;
+
+    private void Start()
+    {
+        _originRotation = transform.localEulerAngles;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (!isRotating)
         {
-            transform.DOLocalRotate(new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - rotateValue, transform.localEulerAngles.z), 0.5f);
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                RotateCamera(0f, - _rotateValue, 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                RotateCamera(0f, _rotateValue, 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                RotateCamera(-10f, 0f, 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                RotateCamera(10f, 0f, 0f);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.DOLocalRotate(new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + rotateValue, transform.localEulerAngles.z), 0.5f);
-        }
+    }
+
+    private void RotateCamera(float x, float y, float z)
+    {
+        isRotating = true;
+        transform.DOLocalRotate(new Vector3(transform.localEulerAngles.x + x, transform.localEulerAngles.y + y, transform.localEulerAngles.z + z), 0.5f)
+            .OnComplete(() =>
+            {
+                isRotating = false;
+            });
     }
 }
