22672a764c24b5d321416e3e82783947ab7fb559
YesMan <kaka0804@naver.com>
2024-11-16 13:24:16 +0900

    Feat: 카메라 컨트롤러 수정(회전 카운트)

--- Assets/02.Scripts/Common/CameraController.cs
+++ Assets/02.Scripts/Common/CameraController.cs
@@ -8,7 +8,12 @@ public class CameraController : MonoBehaviour
     private float _rotateValue = 15f;
     private Vector3 _originRotation;
 
-    private bool isRotating = false;
+    private bool _isRotating = false;
+
+    public int _aRotateCnt = 1;
+    public int _dRotateCnt = 1;
+    public int _sRotateCnt = 1;
+    public int _wRotateCnt = 1;    
 
     private void Start()
     {
@@ -17,34 +22,54 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        if (!isRotating)
+        if (!_isRotating)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                RotateCamera(0f, - _rotateValue, 0f);
+                if (_aRotateCnt >= 1)
+                {
+                    RotateCamera(0f, -_rotateValue, 0f);
+                    _dRotateCnt ++;
+                    _aRotateCnt --;
+                }
             }
             else if (Input.GetKeyDown(KeyCode.D))
             {
-                RotateCamera(0f, _rotateValue, 0f);
+                if (_dRotateCnt >= 1)
+                {
+                    RotateCamera(0f, _rotateValue, 0f);
+                    _aRotateCnt ++;
+                    _dRotateCnt --;
+                }
             }
             else if (Input.GetKeyDown(KeyCode.W))
             {
-                RotateCamera(-10f, 0f, 0f);
+                if (_wRotateCnt >= 1)
+                {
+                    RotateCamera(-10f, 0f, 0f);
+                    _sRotateCnt ++;
+                    _wRotateCnt --;
+                }
             }
             else if (Input.GetKeyDown(KeyCode.S))
             {
-                RotateCamera(10f, 0f, 0f);
+                if (_sRotateCnt >= 1)
+                {
+                    RotateCamera(10f, 0f, 0f);
+                    _wRotateCnt ++;
+                    _sRotateCnt --;
+                }
             }
         }
     }
 
     private void RotateCamera(float x, float y, float z)
     {
-        isRotating = true;
+        _isRotating = true;
         transform.DOLocalRotate(new Vector3(transform.localEulerAngles.x + x, transform.localEulerAngles.y + y, transform.localEulerAngles.z + z), 0.5f)
             .OnComplete(() =>
             {
-                isRotating = false;
+                _isRotating = false;
             });
     }
 }
