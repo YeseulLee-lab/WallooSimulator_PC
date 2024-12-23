4bb45ed34ae85333bc8f7f637c377a74841cc339
YesMan <kaka0804@naver.com>
2024-12-23 20:06:08 +0900

    Feat: 인터랙션 수정

--- Assets/02.Scripts/Interactable/InteractableObject/BagelInteractable.cs
+++ Assets/02.Scripts/Interactable/InteractableObject/BagelInteractable.cs
@@ -14,6 +14,7 @@ public class BagelInteractable : CustomInteractableBase
         GetComponent<MeshRenderer>().enabled = false;
         _ateBagel.SetActive(true);
 
+        WallooManager.instance.isWallooing = false;
         //일정시간 후에 베이글 복원
     }
 
